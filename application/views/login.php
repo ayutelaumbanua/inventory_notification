bb6a9e54f6e03516659137f6740c09ca86e2ceaa
ayutelaumbanua <88342045+ayutelaumbanua@users.noreply.github.com>
2023-03-26 20:41:44 +0700

    add image

--- application/views/login.php
+++ application/views/login.php
@@ -2,20 +2,20 @@
 <html lang="en">
 
 <head>
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#f45">
+	<link rel="shortcut icon" href="assets/img/Logo App.png" type="image/png">
+	<link rel="apple-touch-icon" href="assets/img/icons-192.png" type="image/png">
 	<!-- PWA -->
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
-
 	<title>Inventory Management- Login</title>
 	<!-- PWA -->
-	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<!-- <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
@@ -23,13 +23,12 @@
 	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
 	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
 	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"> -->
 	<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 	<link rel="manifest" href="/manifest.json">
-
 	<link href="<?= base_url('assets') ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link
 		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
@@ -106,6 +105,21 @@
 	<script src="<?= base_url('assets') ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 	<script src="<?= base_url('assets') ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
 	<script src="<?= base_url('assets') ?>/js/bootstrap.min.js"></script>
+	<script>
+		var BASE_URL = '<?= base_url() ?>';
+		document.addEventListener('DOMContentLoaded', init, false);
+
+		function init() {
+			if ('serviceWorker' in navigator && navigator.onLine) {
+				navigator.serviceWorker.register(BASE_URL + 'service-worker.js')
+					.then((reg) => {
+						console.log('Registrasi service worker Berhasil', reg);
+					}, (err) => {
+						console.error('Registrasi service worker Gagal', err);
+					});
+			}
+		}
+	</script>
 </body>
 
 </html>
